5eb9673ee7e9a15d633c68036ff877d2606b64b2
Adrian-Herrera <raiman00@hotmail.com>
2020-12-14 21:47:22 -0400

    Circular Scene Fixed

--- Assets/Scripts/Circular Movement/BaseCircle.cs
+++ Assets/Scripts/Circular Movement/BaseCircle.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class BaseCircle : MonoBehaviour
 {
@@ -9,70 +8,20 @@ public class BaseCircle : MonoBehaviour
 
     private int _vertexNumber = 120;
     private float _radius = 3;
-
-    public GameObject Panel;
-    public GameObject Center;
-    public GameObject Ball;
-    public TMP_InputField[] UIData;
-
-    private float desp_i, desp_f;
-    private float vel_i, vel_f;
-    private float acc_i, acc_f;
-    private float tiempo_i, tiempo_f;
-
-    private bool onMove = false;
-
     private Vector3 centerPos = new Vector3(3f, 0f, 0f);
-    // Start is called before the first frame update
+
     void Start()
     {
         Application.targetFrameRate = 30;
-        Pause();
+
         // Draw();
         lineRenderer = GetComponent<LineRenderer>();
         DrawPolygon(_vertexNumber, _radius, centerPos, 0.15f, 0.15f);
-
-        UIData = Panel.GetComponentsInChildren<TMP_InputField>();
-
-        /*
-
-        [0] = desp_i [1] = desp_f
-        [2] = vel_i [3] = vel_f
-        [4] = acc_i [5] = acc_f
-        [6] = Tiempo_i [7] = Tiempo_f
-
-        */
 
     }
 
     void Update()
     {
-        if (onMove && tiempo_i < 3 - 0.02)
-        {
-
-            
-        }
-        else
-        {
-
-            // Debug.Log(tiempo_i);
-            // Debug.Log(vel_f);
-            onMove = false;
-            Pause();
-
-
-        }
-
-
-    }
-    void FixedUpdate()
-    {
-        if (onMove && tiempo_i < 3 - 0.02)
-        {
-            Vel();
-            TimeCounter();
-
-        }
 
     }
 
@@ -101,81 +50,4 @@ public class BaseCircle : MonoBehaviour
         DrawPolygon(_vertexNumber, _radius, centerPos, 0.2f, 0.2f);
     }
 
-
-    public void Play()
-    {
-        getValues();
-
-
-        onMove = true;
-        Time.timeScale = 1;
-
-    }
-
-    public void Pause()
-    {
-        Time.timeScale = 0;
-        // Debug.Log("Time Scale: 0");
-    }
-
-    public void Resume()
-    {
-        Time.timeScale = 1;
-    }
-
-    public void Reset()
-    {
-        onMove = false;
-        Time.timeScale = 0;
-        Center.transform.rotation = new Quaternion(0, 0, 0, 1);
-        vel_f = vel_i;
-        Center.GetComponent<Rigidbody2D>().angularVelocity = vel_i;
-        tiempo_i = 0;
-        UIData[3].text = "";
-        UIData[6].text = "";
-
-    }
-
-    public void getValues()
-    {
-
-        desp_i = UIData[0].text != "" ? float.Parse(UIData[0].text) : 0;
-        desp_f = UIData[1].text != "" ? float.Parse(UIData[1].text) : 0;
-        vel_i = UIData[2].text != "" ? float.Parse(UIData[2].text) : 0;
-        vel_f = UIData[3].text != "" ? float.Parse(UIData[3].text) : vel_i;
-        acc_i = UIData[4].text != "" ? float.Parse(UIData[4].text) : 0;
-        acc_f = UIData[5].text != "" ? float.Parse(UIData[5].text) : 0;
-        tiempo_i = UIData[6].text != "" ? float.Parse(UIData[6].text) : 0;
-        tiempo_f = UIData[7].text != "" ? float.Parse(UIData[7].text) : 0;
-
-    }
-
-    public void Vel()
-    {
-
-        // Center.transform.Rotate(new Vector3(0, 0, 1), vel_f * Time.deltaTime);
-
-        Center.GetComponent<Rigidbody2D>().angularVelocity = vel_f;
-        Acc();
-        UIData[3].text = vel_f.ToString();
-
-    }
-
-    public void Acc()
-    {
-        if (acc_i != 0)
-        {
-            vel_f += acc_i * Time.deltaTime;
-            // UIData[3].text = vel_f.ToString();
-        }
-    }
-
-    public void TimeCounter()
-    {
-
-        tiempo_i += Time.deltaTime;
-        Debug.Log(tiempo_i);
-        UIData[6].text = tiempo_i.ToString();
-
-    }
 }
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,22 +8,29 @@ public class LevelLoader : MonoBehaviour
     public Animator transition;
     public float transitionTime = 1f;
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        Time.timeScale = 1;
+    }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void LoadNextLevel(string LevelName){
+    public void LoadNextLevel(string LevelName)
+    {
         StartCoroutine(LoadLevel(LevelName));
     }
 
-    IEnumerator LoadLevel(string LevelName){
+    IEnumerator LoadLevel(string LevelName)
+    {
         transition.SetTrigger("Start");
 
         yield return new WaitForSeconds(transitionTime);
