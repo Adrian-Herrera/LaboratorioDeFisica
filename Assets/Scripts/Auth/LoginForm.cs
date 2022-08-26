0edf1b13102ec49749cf39daab0e1e351abe6b90
Adrian-Herrera <raiman00@hotmail.com>
2022-08-26 01:10:31 -0400

    Update obsolete code

--- Assets/Scripts/Auth/LoginForm.cs
+++ Assets/Scripts/Auth/LoginForm.cs
@@ -15,35 +15,32 @@ public class LoginForm : MonoBehaviour
     [SerializeField] GameObject mainMenu, loginMenu;
     IEnumerator Login()
     {
-        WWWForm form = new WWWForm();
+        WWWForm form = new();
         form.AddField("LoginUser", username.text);
         form.AddField("LoginPassword", password.text);
 
         UnityWebRequest www = UnityWebRequest.Post("http://localhost:4000/ingresar", form);
         yield return www.SendWebRequest();
-
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.Log(www.error);
-            Debug.Log(www.downloadHandler.text);
-        }
-        else
+        if (www.result == UnityWebRequest.Result.Success)
         {
             CredentialManager.Current.JwtCredential = JsonUtility.FromJson<JwtCredential>(www.downloadHandler.text);
             UnityWebRequest www2 = UnityWebRequest.Get("http://localhost:4000/usuario/" + CredentialManager.Current.JwtCredential.userId);
             www2.SetRequestHeader("Authorization", "Bearer " + CredentialManager.Current.JwtCredential.token);
             yield return www2.SendWebRequest();
-            if (www2.isNetworkError || www2.isHttpError)
-            {
-                Debug.Log(www2.error);
-                Debug.Log(www2.downloadHandler.text);
-            }
-            else
+            if (www2.result == UnityWebRequest.Result.Success)
             {
                 CredentialManager.Current.UserInfo = JsonUtility.FromJson<UserInfo>(www2.downloadHandler.text);
                 mainMenu.SetActive(true);
                 loginMenu.SetActive(false);
             }
+            else
+            {
+                Helpers.LogNetworkError(www2);
+            }
+        }
+        else
+        {
+            Helpers.LogNetworkError(www);
         }
     }
     public void Submit()
--- Assets/Scripts/Utilities/Helpers.cs
+++ Assets/Scripts/Utilities/Helpers.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Helpers : MonoBehaviour
 {
+    public static void LogNetworkError(UnityWebRequest www)
+    {
+        Debug.Log("Error type: " + www.result + "\nError message: " + www.error + "\nServer message: " + www.downloadHandler.text);
+    }
     public static void ClearListContent<T>(List<T> list) where T : MonoBehaviour
     {
         foreach (T item in list)
