75bbf913bb27fb432f80b88659d14de74aaaf046
Adrian-Herrera <raiman00@hotmail.com>
2023-06-28 19:06:44 -0400

    Change HomeButton script

--- Assets/Code/Scripts/ChangeSceneButton.cs
+++ Assets/Code/Scripts/ChangeSceneButton.cs
@@ -1,11 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeSceneButton : MonoBehaviour
 {
-    public void ChangeScene(string sceneName)
+    private enum SceneName
     {
-        LevelManager.Instance.LoadScene(sceneName);
+        MainMenu,
+        Exercises,
+        Virtual,
+        Calculadora
+    }
+    [SerializeField] private SceneName _changeTo;
+    private Button _button;
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(ChangeScene);
+    }
+    public void ChangeScene()
+    {
+        Debug.Log(_changeTo.ToString());
+        LevelManager.Instance.LoadScene(_changeTo.ToString());
     }
 }
